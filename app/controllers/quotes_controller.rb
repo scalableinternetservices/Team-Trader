01d5371ac1820fcaea0a1be1b1e5407ac28a982e
Sharath Rao <sharathrao13@gmail.com>
2015-10-13 21:59:07 -0700

    Refactored the URL parameters

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,12 @@
 class QuotesController < ApplicationController
   require 'net/http'
   def getHistoricalData()
-url =URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222014-02-11%22%20and%20endDate%20%3D%20%222015-02-18%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
-req=Net::HTTP::Get.new(url.path)
-res=Net::HTTP.start(url.host,url.port){|http|http.request(req)}
-@output = res.body
+    stock_ticker ="YHOO";
+    end_date = "2015-02-18";
+    start_date ="2014-02-11";
+    url =URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22'+stock_ticker+'%22%20and%20startDate%20%3D%20%22'+start_date+'%22%20and%20endDate%20%3D%20%22'+end_date+'%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
+    req=Net::HTTP::Get.new(url.path)
+    res=Net::HTTP.start(url.host,url.port){|http|http.request(req)}
+    @output = res.body
   end
 end
