cf4f84d011ed57f553cb297a3e21b4c18707a7e6
Sharath Rao <sharathrao13@gmail.com>
2015-10-14 22:24:41 -0700

    Using rest client instead of raw http get

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 class QuotesController < ApplicationController
   require 'net/http'
   def getHistoricalData()
@@ -5,8 +7,7 @@ class QuotesController < ApplicationController
     end_date = "2015-02-18"
     start_date ="2014-02-11"
     url =URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22'+stock_ticker+'%22%20and%20startDate%20%3D%20%22'+start_date+'%22%20and%20endDate%20%3D%20%22'+end_date+'%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
-    req=Net::HTTP::Get.new(url.path)
-    res=Net::HTTP.start(url.host,url.port){|http|http.request(req)}
-    @output = res.body
+    response = RestClient.get url
+    @output = response.body
   end
 end
