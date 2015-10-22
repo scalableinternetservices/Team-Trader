f8100cba32737f4a3333f36f5cf80224bfc1cb59
ethanpreble <ethanpreble@gmail.com>
2015-10-22 02:42:03 -0700

    delta n

--- app/controllers/google_trends_strategy_controller.rb
+++ app/controllers/google_trends_strategy_controller.rb
@@ -11,20 +11,44 @@ class GoogleTrendsStrategyController < ApplicationController
     start_date = beginning.to_s#'2015-07-03'
     end_date = today.to_s#'2015-09-30'
 
-    #trend_data = GoogleTrendsService.getWeekly(params[:trend_term])
     trend_data = GoogleTrendsService.getMonths(params[:trend_term], 4)
 
-    @labels = Array.new
-    @values = Array.new
+    @labels = trend_data.keys
+    @values = trend_data.values
+    @values2 = trend_data.values
+    @values2 = get_array_of_relative_search @values
 
-    trend_data.each do |val|
-      @labels.insert(0,val[0])
-      @values.insert(0,val[1])
-    end
+  #@labels.reverse!
+  #@values.reverse!
 
   #returnArray = QuandlQuoteService.getData(params[:stock_symbol], start_date, end_date, 'daily')
   #@labels = returnArray[0]
   #@values = returnArray[1]
+  end
+
+  private
+
+  def delta_t
+    3
+  end
+
+  def get_array_of_relative_search(values)
+    rel = Array.new(values.size,0)
+
+    values.each_with_index do |val, index|
+      if((index > 2) && index!=values.size-1)
+        rel[index] = relative_change_in_search(index, values)
+        
+      end
+    end
+    logger.info rel
+    logger.info @values
+    rel
+  end
+
+  def relative_change_in_search(index, values)
+    m = values[index-delta_t,index-1].mean
+    values[index] - m
   end
 
 end
---
+++ test/lib/rails_extensions_test.rb
@@ -0,0 +1,11 @@
+require '../test_helper'
+
+class RailsExtensionsTest < ActiveSupport::TestCase
+  def test_array_mean
+    assert_equal 18.75, [17, 22, 12, 24].mean
+  end
+  
+  def test_array_mean_0
+    assert_equal 0, [0, 0, 0, 0].mean
+  end
+end
