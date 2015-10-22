19245f4aa5ce83d12fa65ba4d142a27fbb813606
ethanpreble <ethanpreble@gmail.com>
2015-10-22 00:51:10 -0700

    fix params for google trend

--- test/controllers/google_trends_strategy_controller_test.rb
+++ test/controllers/google_trends_strategy_controller_test.rb
@@ -7,7 +7,7 @@ class GoogleTrendsStrategyControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
-    hash = {:start_date=>'2014-04-20', :end_date=>'2014-07-05', :collapse => 'weekly', :stock_symbol => 'INDEX_DJI'}
+    hash = {:start_date=>'2014-04-20', :end_date=>'2014-07-05', :collapse => 'weekly', :stock_symbol => 'INDEX_DJI', :trend_term =>'GOOG'}
         # start_date=2014-04-20&end_date=2014-07-10&collapse=weekly&ticker=INDEX_DJI
     get(:show, hash)
     assert_response :success
