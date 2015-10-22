7f5d77e540c755854ab7e182fe8480a3b4f6ae14
Mathieu Rodrigue <mathieu@169-231-148-90.wireless.ucsb.edu>
2015-10-22 12:13:23 -0700

    add volume chart, google trends correlation

--- app/controllers/volume_chart_controller.rb
+++ app/controllers/volume_chart_controller.rb
@@ -1,9 +1,11 @@
 class VolumeChartController < ApplicationController
   require 'rest-client'
   require 'json'
+  require 'google_trends_service'
   def index
   	# configure
-	start_date = params[:start_date]
+    
+	  start_date = params[:start_date]
     end_date = params[:end_date]
     url = "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_DJI.json"
     params = {:params => {'start_date'=> start_date, 'end_date'=> end_date}}
@@ -12,15 +14,24 @@ class VolumeChartController < ApplicationController
     @result_json = JSON.parse(result)
     @result_json = @result_json['dataset']['data']
 
+    # @trends = GoogleTrendsService.getDaily("debt")
+
     @labels = Array.new
     @values = Array.new
+    @trend_labels = Array.new
+    @trend_values = Array.new
 
     @result_json.each do |val|
-      puts val
       @labels.insert(0,val[0])
       @values.insert(0,val[5])
     end
 
-    return @labels, @values
+    # @trends.each do |key, value|
+    #   if @trend_values.length < @values.length
+    #     @trend_values.insert(0, value)
+    #   end
+    # end
+
+    return @labels, @values, @trend_values
   end
 end
