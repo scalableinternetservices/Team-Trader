a50d72e368bbab75853080b22aab17d2eb3d867e
ethanpreble <ethanpreble@gmail.com>
2015-10-22 00:46:05 -0700

    fix google parser

--- app/controllers/google_trends_strategy_controller.rb
+++ app/controllers/google_trends_strategy_controller.rb
@@ -10,10 +10,21 @@ class GoogleTrendsStrategyController < ApplicationController
     beginning = today-365#1095
     start_date = beginning.to_s#'2015-07-03'
     end_date = today.to_s#'2015-09-30'
- 
-    returnArray = QuandlQuoteService.getData(params[:stock_symbol], start_date, end_date)
-    @labels = returnArray[0]
-    @values = returnArray[1]
+
+    #trend_data = GoogleTrendsService.getWeekly(params[:trend_term])
+    trend_data = GoogleTrendsService.getMonths(params[:trend_term], 4)
+
+    @labels = Array.new
+    @values = Array.new
+
+    trend_data.each do |val|
+      @labels.insert(0,val[0])
+      @values.insert(0,val[1])
+    end
+
+  #returnArray = QuandlQuoteService.getData(params[:stock_symbol], start_date, end_date, 'daily')
+  #@labels = returnArray[0]
+  #@values = returnArray[1]
   end
 
 end
