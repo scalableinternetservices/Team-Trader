40b794aa669ea970ec23af5cadb867725a872922
ethanpreble <ethanpreble@gmail.com>
2015-10-21 23:05:49 -0700

    making google_trends service use <<self

--- app/services/google_trends_service.rb
+++ app/services/google_trends_service.rb
@@ -4,30 +4,31 @@ class APIError < StandardError
 end
 
 class GoogleTrendsService
-     
-  def self.getDaily(term) 
-    get(term, 'today+3-m')
+  class <<self
+    def getDaily(term)
+      get(term, 'today+3-m')
+    end
+
+    def getWeekly(term)
+      get(term, 'today+36-m')
+    end
+
+    private
+
+    def get(term, date)
+      request = url + common_params + '&q=' + term + '&date=' + date
+      response = RestClient.get(request, {'Cache-Control' => 'max-age=0' })
+      raise APIError unless response.code == 200
+      GoogleTrendsParser.parse(response)
+    end
+
+    def url
+      'http://www.google.com/trends/fetchComponent'
+    end
+
+    def common_params
+      '?cid=TIMESERIES_GRAPH_0&export=3&geo=US'
+    end
   end
-  
-  def self.getWeekly(term)
-    get(term, 'today+36-m')
-  end
-   
-  private
-  
-  def self.get(term, date)
-    request = url + common_params + '&q=' + term + '&date=' + date
-    response = RestClient.get(request, {'Cache-Control' => 'max-age=0' })
-    raise APIError unless response.code == 200
-    GoogleTrendsParser.parse(response)
-  end
-  
-  def self.url
-    'http://www.google.com/trends/fetchComponent'
-  end
-  
-  def self.common_params
-    '?cid=TIMESERIES_GRAPH_0&export=3&geo=US'
-  end
-      
+
 end
