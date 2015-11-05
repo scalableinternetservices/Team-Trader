e62994ef03d83acf6ecedb1555e6dffc96961ba8
wangyuesong <573978301@qq.com>
2015-11-04 17:06:58 -0800

    Fix again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027001617) do
+ActiveRecord::Schema.define(version: 20151101000342) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "code"
