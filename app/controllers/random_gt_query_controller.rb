6036fd937bef80d4c8cb667062fe79c13ccc800e
ethanpreble <ethanpreble@gmail.com>
2015-11-19 10:17:58 -0800

    remove puts

--- app/controllers/random_gt_query_controller.rb
+++ app/controllers/random_gt_query_controller.rb
@@ -3,16 +3,12 @@ class RandomGtQueryController < ApplicationController
 
 	offset = rand(TermSearchHistory.count)
 	t = TermSearchHistory.offset(offset).first
-  	puts t.term
   	redirect_to controller: 'volume_chart', action: 'show', stock_symbol: 'Apple Inc.(AAPL)', trend_term: t.term
-
   end
 
   def gts
   	offset = rand(TermSearchHistory.count)
 	t = TermSearchHistory.offset(offset).first
-  	puts t.term
   	redirect_to controller: 'google_trends_strategy', action: 'show', stock_symbol: 'Apple Inc.(AAPL)', trend_term: t.term
-
   end
 end
