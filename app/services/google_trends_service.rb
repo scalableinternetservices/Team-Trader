67ba3d0c70d53301e30972523e17af8b8c213442
ethanpreble <ethanpreble@gmail.com>
2015-12-03 17:29:18 -0800

    adding multiple nid cookies

--- app/services/google_trends_service.rb
+++ app/services/google_trends_service.rb
@@ -36,10 +36,18 @@ class GoogleTrendsService
 
     def cook
       {
-        'NID'=>'72=dOq_sKa79YxKWqSsNtqkpSD4OzuljNH1L79xfwN-BVS4Av-HlE9guDkRlfsBe2t2Ooiby3mNCjie0JdZxnWNcPs8v2eW0qRnXofJvNCajJ2sSiTvLFVMWQSAahbgXbmJ'
+        'NID'=>nid.sample
       }
     end
-
+    
+    def nid
+      [
+        '72=dOq_sKa79YxKWqSsNtqkpSD4OzuljNH1L79xfwN-BVS4Av-HlE9guDkRlfsBe2t2Ooiby3mNCjie0JdZxnWNcPs8v2eW0qRnXofJvNCajJ2sSiTvLFVMWQSAahbgXbmJ',
+        '74=FzM5QkbxG76Vj8JIxAfNTDOKkdJ696-25jJiWS2DxO7jLK1PGrSnrc4dzPqE8BDO1o5y2_tClIm16xi-EisSM11Dy3gpipXd63qTiUq-y37A763XIuI3f9cgt9fpx0_31Us8flq-C_RY1mkn0lByT7VOweyKFfj6rQbraeX7pmIkh71TKw0BqJnPsqYcvH9KRh1c',
+        '74=aEbh5-sgUfDcdId0QS0yajdi09Xds1WNiltLZFtlDDm5jwo7NXjHPE6oLW_nNGLDLtfYkqoJlVAnE8RLHbo30ivVTcQkjnLfOFeDyJR5Th8jaRnWcB_EhFfd6R8NdMexxSiMXrFQvA2ECMoI8jXc66CmvIe7SjWl4QG-4ayPmpc_AzoHt7oi46-IOpTKlu6zGJkfQg'
+      ]
+    end
+    
     def get_data(url)
       puts url
       Rails.cache.fetch(url, :expires =>12.hours) do
