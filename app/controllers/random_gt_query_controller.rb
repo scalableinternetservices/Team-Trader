b9604d27148e7bc9527884bc39fde2eb3687c316
ethanpreble <ethanpreble@gmail.com>
2015-12-05 14:38:26 -0800

    make random queries constrained

--- app/controllers/random_gt_query_controller.rb
+++ app/controllers/random_gt_query_controller.rb
@@ -1,14 +1,27 @@
 class RandomGtQueryController < ApplicationController
   def volume
-
-	offset = rand(TermSearchHistory.count)
-	t = TermSearchHistory.offset(offset).first
-  	redirect_to controller: 'volume_chart', action: 'show', stock_symbol: 'Apple Inc.(AAPL)', trend_term: t.term
+  	redirect_to controller: 'volume_chart', action: 'show', stock_symbol: rand_stock, trend_term: rand_term
   end
 
   def gts
-  	offset = rand(TermSearchHistory.count)
-	t = TermSearchHistory.offset(offset).first
-  	redirect_to controller: 'google_trends_strategy', action: 'show', stock_symbol: 'Apple Inc.(AAPL)', trend_term: t.term
+  	redirect_to controller: 'google_trends_strategy', action: 'show', stock_symbol: rand_stock, trend_term: rand_term
+  end
+  
+  def rand_term
+    terms.sample
+  end
+  
+  def rand_stock
+    stocks.sample
+  end
+  
+  def terms
+    ['southwest', 'expectations', 'independence', 'knowing', 'debt', 'stock', 'portfolio', 'investment', 'hedge', 'economics', 'markets', 'bond', 'kangaroo', 'navigator', 'tablespoon']
+  end
+  
+  def stocks
+    ['Apple Inc.(AAPL)', 'Alphabet Inc.(GOOG)', 'Microsoft Corporation(MSFT)', 'Yahoo! Inc.(YHOO)', 
+      'Alphabet Inc.(GOOGL)', 'Big 5 Sporting Goods Corporation(BGFV)', 'Bank of America Corporation(BAC)', 
+      'Intel Corporation(INTC)', 'Chase Corporation(CCF)']
   end
 end
