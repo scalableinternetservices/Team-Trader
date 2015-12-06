5f08089cffe3e52085ff79d9df93cae2d5746530
wangyuesong <573978301@qq.com>
2015-12-06 11:04:53 -0800

    Russian doll

--- app/helpers/indices_helper.rb
+++ app/helpers/indices_helper.rb
@@ -1,2 +1,9 @@
 module IndicesHelper
+  def cache_key_for_investment(inv)
+    "investments/#{inv.id}/#{inv.updated_at}"
+  end
+
+  def cache_key_for_investments(investments)
+    "investments/#{investments.maximum(:updated_at)}"
+  end
 end
